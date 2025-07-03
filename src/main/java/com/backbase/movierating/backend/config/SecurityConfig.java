71c66ec8e222f39d7ad50091f8d638b48db6f521
Zia <zia7770@yahoo.com>
2025-07-03 06:58:29 +0200

    Fix Authorize button in swagger page

--- src/main/java/com/backbase/movierating/backend/config/SecurityConfig.java
+++ src/main/java/com/backbase/movierating/backend/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.backbase.movierating.backend.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -57,4 +61,17 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+
 }
